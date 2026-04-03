c6eba792b466f9969a51fdf06387ce4f0edab455
JOBINJOSEPH034 <jobinjoseph034@gmail.com>
2026-04-03 22:13:47 +0530

    fix(build): implement foolproof virtual environment for PEP 668 compliance

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt --break-system-packages
+echo "Creating virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
 
+echo "Installing requirements..."
+pip install -r requirements.txt
+
+echo "Collecting static files and migrating database..."
 python manage.py collectstatic --no-input
 python manage.py migrate
